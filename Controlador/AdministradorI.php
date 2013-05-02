197ca64634c5b2f8e6ae50a9c643c3a56cd73177
saixenia <julian-rodriguez92@hotmail.com>
2013-05-02 01:51:13 -0500

    decodificacion

--- Controlador/AdministradorI.php
+++ Controlador/AdministradorI.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html;charset=ISO-8859-1');
 error_reporting(0);
 include '../Modelo/Clase_SAdministrativoI.php';
 $vccontrol = $_POST["boton"];
-$valores = $_POST["valores"];
+$valores = utf8_decode($_POST["valores"]);
 $a = new SAdministrativoI();
 switch ($vccontrol) {
     case "1": {
